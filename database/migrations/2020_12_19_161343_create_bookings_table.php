e5d6c13539b32e2142c94eb855725637f67412e3
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2020-12-19 22:39:07 +0600

    Database, tables, models, controllers created, JWT Auth integrated

---
+++ database/migrations/2020_12_19_161343_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_number');
+            $table->dateTime('arrival');
+            $table->dateTime('checkout');
+            $table->foreignId('customer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('book_type');
+            $table->dateTime('book_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
